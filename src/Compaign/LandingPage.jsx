e889d41a590c75723a4c5e43aba8cabbfea4a659
unknown <jjhdigitalmedia@gmail.com>
2025-05-28 12:04:19 +0530

    ff

--- src/Compaign/LandingPage.jsx
+++ src/Compaign/LandingPage.jsx
@@ -8,6 +8,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import PromoAdF2 from "../assets/PromoAdF2.png";
 import PromoAd from "../assets/PromoAd.jpg";
 import LogoAB from '../assets/LogoAB.png'
+import DrVandana from '../assets/DrVandana.jpg'
 import "../Style/Style.css";
 
 const LandingPage = () => {
@@ -73,14 +74,13 @@ const LandingPage = () => {
   return (
     <div className="min-h-screen bg-white text-gray-800">
       {/* Header */}
-      <header className="bg-rose-100 shadow-md text-black sticky top-0 z-20 py-2 px-4 text-center text-xl font-bold">
-
-        <ul className="flex flex-wrap md:justify-between md:mx-72">
-          <li className="text-rose-700"><img src={LogoAB} className="w-60" alt="" /></li>
+      <header className="bg-rose-100 shadow-md text-black sticky top-0 z-20 py-3 px-4 text-center text-xl font-bold">
+        <ul className="flex flex-wrap md:justify-between text-center md:mx-72">
+          <li className="text-rose-700"><img src={LogoAB} className="w-40 md:w-60" alt="" /></li>
           <li className="invisibl py-3">
-            <IoCallOutline className="inline mr-2 text-3xl" />
-            <a href="tel:9151037784"> +91 9151037784 </a></li>
-          <li className="py-3">
+            <IoCallOutline className="md:inline hidden mr-2 text-3xl" />
+            <a className="hidden" href="tel:9151037784"> +91 9151037784 </a></li>
+          <li className="hidden py-3">
             <button className="bg-green-500 align-middle text-white p-2 mr-2 rounded-full shadow-lg hover:bg-red-700 transition-all">
               <a href="https://wa.me/message/JG2WQ7ZXYWU6B1" className="" target="_blank"><FaWhatsapp size={20} /></a>
             </button>
@@ -91,16 +91,16 @@ const LandingPage = () => {
 
       {/* Introduction Section........................................... */}
 
-
-      <div className="relative h-auto mx-auto overflow-hidden">
-        {/* Left Arrow Button */}
+      <div>
+        <img src={PromoAdF2} alt="" />
+      </div>
+      {/* <div className="relative h-auto mx-auto overflow-hidden">
         <button
           className="absolute left-2 top-1/2 transform -translate-y-1/2 text-white bg-black z-10 bg-opacity-50 p-2 rounded-full text-xl"
           onClick={prevImage}
         >
           ‹
         </button>
-        {/* Image Container with Sliding Effect */}
         <div
           className="flex transition-transform  duration-500 ease-in-out"
           style={{
@@ -117,7 +117,6 @@ const LandingPage = () => {
             </div>
           ))}
         </div>
-        {/* Right Arrow Button */}
         <button
           className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white bg-black bg-opacity-50 p-2 rounded-full text-xl"
           onClick={nextImage}
@@ -125,7 +124,6 @@ const LandingPage = () => {
           ›
         </button>
 
-        {/* Dot Indicators at the Bottom */}
         <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
           {images.map((_, index) => (
             <button
@@ -135,28 +133,28 @@ const LandingPage = () => {
             />
           ))}
         </div>
-      </div>
+      </div> */}
 
-      <section className="w-full px-40 mx-auto items-center justify-between flex flex-wrap py-8 text-center bg-gray-100">
-        <div className="mx-20">
-          <h1 className="text-3xl font-bold mb-2">Happy Couples</h1>
-          <p className="text-lg">10,000+</p>
+      <div className="flex flex-wrap mb-20 bg-gray-200">
+        <div className="px-6 py-3 md:py-10 mx-auto text-center">
+          <h1 className="md:text-3xl font-bold mb-2">Happy Couples</h1>
+          <p className="md:text-lg">20,000+</p>
         </div>
-        <div className="mx-20">
-          <h1 className="text-3xl font-bold mb-2">Success Rate</h1>
+        <div className="px-6 py-3 md:py-10 mx-auto text-center">
+          <h1 className="md:text-3xl font-bold mb-2">Success Rate</h1>
           <p className="text-lg">70%+</p>
         </div>
-        <div className="mx-20">
-          <h1 className="text-3xl w-fit font-bold mb-2">IVF Budget</h1>
-          <p className="text-lg">Every month broke my heart. failure.</p>
+        <div className="px-6 py-3 md:py-10 mx-auto text-center">
+          <h1 className="md:text-3xl font-bold mb-2">Years of Experience</h1>
+          <p className="text-lg">38+</p>
         </div>
-      </section>
+      </div>
 
       <section id='contact' className="text-gray-600 body-font relative">
-        <div className="container px-5 py-12 mx-auto">
+        <div className="container md:px-5 py-12 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-5xl text-2xl font-medium title-font mb-4 text-gray-600 font-asap">Book Your Appointment</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-bold">If you're seeking treatment or have any queries, feel free to submit this form. I'll get back to you as soon as possible.</p>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-bold">If you'd like to book an appointment, please fill out this form. I’ll get back to you as soon as possible to confirm your booking</p>
           </div>
           <div className="lg:w-1/2 md:w-2/3 mx-auto">
             <form onSubmit={onSubmit} className="flex flex-wrap -m-2">
@@ -196,16 +194,72 @@ const LandingPage = () => {
 
       {/* Details Section............................................... */}
 
-
-      <section className="px-6 py-10 w-4/5 mx-auto ">
-        <h2 className="text-2xl font-semibold mb-4">Why Choose Indira US?</h2>
-        <ul className="list-disc list-inside space-y-2 text-lg">
-          <li>India’s Top IVF centre with 70%+ Success Rate centers</li>
-          <li>Advanced technology & experienced doctors</li>
-          <li>Personalized fertility plans</li>
-          <li>Transparent pricing</li>
-        </ul>
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-col">
+          <div class="lg:w-4/6 mx-auto">
+            {/* <div class="rounded-lg h-64 overflow-hidden">
+        <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500"/>
+      </div> */}
+            <div class="flex flex-col sm:flex-row mt-10">
+              <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                  <img src={DrVandana} alt="" />
+                </div>
+                <div class="flex flex-col items-center text-center justify-center">
+                  <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Dr Vandana Bansal</h2>
+                  <div class="w-12 h-1 bg-rose-500 rounded mt-2 mb-4"></div>
+                  <p class="text-base font-bold"> MBBS, MS (Obstetrics & Gynaecology) |
+                    Director & Chief Fertility Consultant | Laparoscopic & Hysteroscopic Surgeon</p>
+                </div>
+              </div>
+              <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                <p class="leading-relaxed text-lg text-justify mb-4"> Dr. Vandana Bansal is a leading name in the field of infer lity treatment and women's health. With over
+                  38+ years of experience, she has helped thousands of couples fulfil their dream of parenthood. Known for her empathe c counselling, me culous approach, and high IVF success rates, she is deeply commi ed to ethical and pa ent-centric care</p>
+                <p class="leading-relaxed text-justify text-lg mb-4">Welcome to Advanced Fer lity & IVF Centre
+                  We are a state-of-the-art facility dedicated to helping
+                  couples achieve their dream of parenthood. With
+                  world-class technology, a compassionate team, and
+                  decades of experience, we offer personalized fer lity
+                  care for every hopeful parent.</p>
+                {/* <a class="text-indigo-500 inline-flex items-center">Learn More
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
+
+      {/* ................................... */}
+
+      <div className="flex flex-wrap my-28">
+        <section className="px-6 py-10 mx-auto ">
+          <h2 className="text-2xl font-semibold mb-4">Why Choose Indira US?</h2>
+          <ul className="list-disc list-inside space-y-2 text-lg">
+            <li>India’s Top IVF centre with 70%+ Success Rate centers</li>
+            <li>Advanced technology & 38+ Years of Experience</li>
+            <li>Personalized fertility plans</li>
+          </ul>
+        </section>
+        <section className="px-6 py-10 mx-auto ">
+          <h2 className="text-2xl font-semibold mb-4">Compassionate Care That Matters</h2>
+          <ul className="list-disc list-inside space-y-2 text-lg">
+            <li>Dedicated fertility counsellors for emotional support</li>
+            <li>Listening actively and understanding patients' emotional and physical experiences.</li>
+            <li>Women-led clinical team focused on comfort & privacy</li>
+          </ul>
+        </section>
+        <section className="px-6 py-10 mx-auto ">
+          <h2 className="text-2xl font-semibold mb-4">Our Commitment to You</h2>
+          <ul className="list-disc list-inside space-y-2 text-lg">
+            <li>We walk with you — every step, every emotion</li>
+            <li>No shortcuts. No false hopes. Just honest care</li>
+            <li>24x7 patient support that listens and understands</li>
+          </ul>
+        </section>
+      </div>
 
       {/* Appointment .......................................... */}
 
@@ -214,7 +268,7 @@ const LandingPage = () => {
 
       {/* Footer */}
       <footer className="bg-gray-200 text-center text-sm text-gray-700 py-4">
-        &copy; {new Date().getFullYear()} Indira IVF. All rights reserved.
+        &copy; {new Date().getFullYear()} Arpit Test Tube Baby Centre, All rights reserved.
       </footer>
     </div>
   );
