d375b07597210b059a16b903da6f54d45301c611
unknown <jjhdigitalmedia@gmail.com>
2025-06-11 17:15:53 +0530

    ff

--- src/Component/Appointment.jsx
+++ src/Component/Appointment.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Appointment = () => {
   return (
-    <div className="bg-gray-100 flex items-center flex-wrap md:px-16 py-12 w-full m-auto ">
+    <div className="bg-gray-100 flex items-center flex-wrap md:px-16 py-10 w-full m-auto ">
 
-      <div className="text-center md:text-left md:w-1/2 w-full md:py-10 px-4 md:px-10">
+      <div className="text-center md:text-left md:w-1/2 w-full md:py-10 px-2 md:px-10">
         <h1 className=' text-4xl text-centre md:text-left md:text-6xl mb-2 text-gray-600 font-bold font-asap md:mt-4'>Request an Appointment</h1>
         <span className='text-2xl md:text-left block text-gray-500 font-asap mt-2'>Don't Wait, Consult with Dr. Vandana Bansal Today!</span>
       </div>
 
-      <div className="md:text-right md:w-1/2 w-full px-4">
-        <button className='bg-rose-700 text-white py-3 md:w-96 w-40 mx-auto rounded-xl md:text-2xl px-10'><Link to='/book-appointment'> Book Now</Link></button>
+      <div className="md:text-right md:w-1/2 w-full text-center my-8 px-4">
+        <button className='bg-rose-700 text-white py-3 md:w-96 w-60 mx-auto rounded-xl md:text-2xl px-10'><Link to='/book-appointment'> Book Now</Link></button>
       </div>
 
     </div>
--- src/Component/Arrow.jsx
+++ src/Component/Arrow.jsx
@@ -29,7 +29,7 @@ const Arrow = () => {
       {visible && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-24 right-6 bg-rose-700 text-white p-3 rounded-full shadow-lg hover:bg-rose-800 transition-all"
+          className="fixed bottom-24 z-50 right-6 bg-rose-700 text-white p-3 rounded-full shadow-lg hover:bg-rose-800 transition-all"
         >
           <FaArrowUp size={20} />
         </button>
